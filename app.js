221ddd3447408d797cf3c1dc099a2ffe4fb9e655
leocolodro <leocolodro@gmail.com>
2022-04-20 21:08:45 -0300

    Segundo commit

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ const app = express();
 app.use(express.static('public'));
 
 
-app.listen(3000, ()=>{
-    console.log('Servidor funcionando');
-});
+// app.listen(3000, ()=>{
+//     console.log('Servidor funcionando');
+// });
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Servidor corriendo en el puerto ", process.env.PORT );
+})
 
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/home.html');
